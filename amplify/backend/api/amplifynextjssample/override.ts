21605bb78acc8c68d2342a0f625a311d9816228a
tamutamu <tamu.0.0.tamu@gmail.com>
2022-08-01 20:47:29 +0900

    fix error.

--- amplify/backend/api/amplifynextjssample/override.ts
+++ amplify/backend/api/amplifynextjssample/override.ts
@@ -1,14 +1,16 @@
 import { AmplifyApiGraphQlResourceStackTemplate } from "@aws-amplify/cli-extensibility-helper";
 
 export function override(resources: AmplifyApiGraphQlResourceStackTemplate) {
-  const model = resources.models["Post"];
-  const functionKey =
-    "MutationcreatePostinit1FunctionMutationcreatePostinit1Function.AppSyncFunction";
+  if (resources.models) {
+    const model = resources.models["Post"];
+    const functionKey =
+      "MutationcreatePostinit1FunctionMutationcreatePostinit1Function.AppSyncFunction";
 
-  //   console.log(functionKey);
-  //   console.log(model.appsyncFunctions);
-  //   console.log(model.appsyncFunctions[functionKey]);
+    //   console.log(functionKey);
+    //   console.log(model.appsyncFunctions);
+    //   console.log(model.appsyncFunctions[functionKey]);
 
-  model.appsyncFunctions[functionKey].dataSourceName =
-    "CommonValidatorLambdaDataSource";
+    model.appsyncFunctions[functionKey].dataSourceName =
+      "CommonValidatorLambdaDataSource";
+  }
 }
