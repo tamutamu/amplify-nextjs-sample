87bac412d57f2b1968c5055a2053d43e4f1715e7
tamutamu <tamu.0.0.tamu@gmail.com>
2022-08-06 08:10:39 +0900

    no ssr?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Amplify } from "aws-amplify";
 import config from "../src/aws-exports";
-Amplify.configure({ ...config, ssr: true });
+Amplify.configure({ ...config, ssr: false });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
